b9335b7ef29c42494dc88e93644da567d9e472ad
Almighty_Anubis <dereinstein80@gmail.com>
2023-09-27 19:27:54 +0200

    Einzellogo Skript

---
+++ Beyrau/skript.sh
@@ -0,0 +1,7 @@
+inkscape Konzept_Beyrau.svg --export-id="VTU" --export-filename="VTU.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="BSYT" --export-filename="BSYT.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="WVET" --export-filename="WVET.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="CIW" --export-filename="CIW.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="SGA" --export-filename="SGA.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="EPT" --export-filename="EPT.png" --export-type="png" 2> /dev/null
+inkscape Konzept_Beyrau.svg --export-id="Fakultaet" --export-filename="Fakultaet.png" --export-type="png" 2> /dev/null
